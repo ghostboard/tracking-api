337842dd94aeb13837bb7745c4b381fc0c16450c
David Burgos <david.burgos.risco@gmail.com>
2021-10-02 19:35:10 +0200

    Remove socket queue

--- src/lib/cache/live/onAddView.ts
+++ src/lib/cache/live/onAddView.ts
@@ -4,18 +4,32 @@ import { client as cache } from '../../../sources/redis'
 export default async function onAddView(view: any) {
     const { 
         blog: blogId, 
-        visit: viewId
+        visit: viewId,
+        mobile,
+        url,
+        country,
+        referer
     } = view as any;
     try {
         const done = await setView(view)
         if (done) {
-            cache.zadd(`live:blog:${blogId}:queue:add`, 'NX', new Date().getTime(), viewId, (e, reply) => {
-                const ready = !e && reply
-                if (ready) {
-                    cache.publish('live:queue:add', blogId)
-                }
-                return ready
-            })
+            const transactions: any[] = [];
+            transactions.push(["incr", `live:blog:${blogId}:count:total`])
+            if (mobile) {
+                transactions.push(["incr", `live:blog:${blogId}:count:mobile`])
+            }
+            if (url) {
+                transactions.push(["zincrby", `live:blog:${blogId}:urls`, 1, url])
+            }
+            if (country) {
+                transactions.push(["zincrby", `live:blog:${blogId}:countries`, 1, country])
+            }
+            if (referer) {
+                transactions.push(["zincrby", `live:blog:${blogId}:referers`, 1, referer])
+            }
+            transactions.push(["zrem", `live:blog:${blogId}:queue:add`, viewId])
+            const done = cache.multi(transactions).exec()
+            return done
         }
     } catch (e) {
         console.error('>> redis error onAddView', e);
--- src/lib/cache/live/onQuitView.ts
+++ src/lib/cache/live/onQuitView.ts
@@ -1,14 +1,39 @@
 import { client as cache } from '../../../sources/redis'
+import getView from './getView'
 
 export default async function onQuitView(blogId: string, viewId: string) {
     try {
-        cache.zadd(`live:blog:${blogId}:queue:quit`, 'NX', new Date().getTime(), viewId, (e, reply) => {
-            const ready = !e && reply
-            if (ready) {
-                cache.publish('live:queue:quit', blogId)
-            }
-            return ready
-        })
+        const view: any = await getView(viewId)
+        if (!view) {
+            return { done: false }
+        }
+        const { url, country, referer, mobile } = view
+        const blogId = view.blog;
+
+        if (!blogId) {
+            return { done: false }
+        }
+        const transactions: any[] = [];
+        transactions.push(["decr", `live:blog:${blogId}:count:total`])
+        if (mobile) {
+            transactions.push(["decr", `live:blog:${blogId}:count:mobile`])
+        }
+        if (url) {
+            transactions.push(["zincrby", `live:blog:${blogId}:urls`, -1, url])
+        }
+        if (country) {
+            transactions.push(["zincrby", `live:blog:${blogId}:countries`, -1, country])
+        }
+        if (referer) {
+            transactions.push(["zincrby", `live:blog:${blogId}:referers`, -1, referer])
+        }
+        transactions.push(["del", `live:view:${viewId}`])
+        transactions.push(["del", `live:view:copy:${viewId}`])
+        transactions.push(["del", `view:${viewId}`])
+        transactions.push(["zrem", `live:blog:${blogId}:queue:add`, viewId])
+        transactions.push(["zrem", `live:blog:${blogId}:queue:quit`, viewId])
+        const done = cache.multi(transactions).exec()
+        return done
     } catch (e) {
         console.error('>> redis error onQuitView', e);
         return false;

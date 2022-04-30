68690af3fbd3de6c39f2de1cc341f7735336c2cc
David Burgos <david.burgos.risco@gmail.com>
2022-04-30 10:31:26 +0200

    Fix hasNewsletter for no-ghost generator

--- src/lib/blog/hasMailgunNewsletter.ts
+++ src/lib/blog/hasMailgunNewsletter.ts
@@ -1,4 +1,9 @@
 export default function(generator: String=''): boolean {
+		const isGhost = generator.includes('Ghost ');
+		if (!isGhost) {
+			// Gatsby or another headless UI use Content API
+			return true;
+		}
     let rawVersion = generator.replace("Ghost ", "").replace(".", "");
     if (rawVersion.length === 2) {
         rawVersion += '0';
